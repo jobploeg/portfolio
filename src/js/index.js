3a7009161dd9e1a18ba58fe7d0b83e930088a924
jploeg <jploeg3450@student.landstede.nl>
2022-10-19 11:15:31 +0200

    Added animations and text to homepage

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-
 // If screen == lg, toggle the class "hidden" on the nav
 window.addEventListener('resize', function(event) {
     if (window.innerWidth >= 1024 &&  document.querySelector(".header").classList.contains("hidden")) {
@@ -10,16 +9,4 @@ window.addEventListener('resize', function(event) {
 function openHeader() {
     document.querySelector(".header").classList.toggle("hidden");
 }
-
-//Text animation
-document.addEventListener("DOMContentLoaded", function(){
-    var replacers = document.querySelectorAll('[data-replace]');
-    for(var i=0; i<replacers.length; i++){
-        let replaceClasses = JSON.parse(replacers[i].dataset.replace.replace(/'/g, '"'));
-        Object.keys(replaceClasses).forEach(function(key) {
-            replacers[i].classList.remove(key);
-            replacers[i].classList.add(replaceClasses[key]);
-        });
-    }
-});
 
